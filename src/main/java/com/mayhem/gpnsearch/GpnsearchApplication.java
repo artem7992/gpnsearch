4bea5556efa2e684f67fab427fdd558e6caaa977
Artem Semenyuk <artemartem7992@gmail.com>
2021-08-19 20:00:47 +0300

    init commit

---
+++ src/main/java/com/mayhem/gpnsearch/GpnsearchApplication.java
@@ -0,0 +1,13 @@
+package com.mayhem.gpnsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpnsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpnsearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mayhem/gpnsearch/GpnsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayhem.gpnsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpnsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
