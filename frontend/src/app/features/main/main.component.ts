be6bd7ae5dc3dca562d453f69afa6c894ac2b6d4
Artem Semenyuk <artemartem7992@gmail.com>
2021-08-26 22:22:03 +0300

    added simple input without style

---
+++ frontend/src/app/features/main/main.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gnpsearch-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent {
+
+}
